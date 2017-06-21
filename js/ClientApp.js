a9f2f857887e358be6bf1abf4b03939201be022b
Trina Haque <trina_haque28@outlook.com>
2017-06-21 15:59:19 -0700

    props and factories

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -6,22 +6,25 @@ var MyTitle = React.createClass({
   render: function(){
     return (
       div(null,
-        h1(null, "Check out this component")
+        h1({style: {color: this.props.color, fontWeight: 'bold'}}, this.props.title)
       )
     )
   }
 })
 
+
+var MyTitleFactory = React.createFactory(MyTitle)
+
 var MyFirstComponent = React.createClass({
   // createClass is blueprint of new type of component
   render: function () {
     return (
-
       div(null, [
-          React.createElement(MyTitle),
-          React.createElement(MyTitle),
-          React.createElement(MyTitle),
-          React.createElement(MyTitle)
+          // React.createElement(MyTitle, null), MyTitleFactory replaces that
+          MyTitleFactory({title: "what is prop?", color: 'rebeccapurple'}),
+          MyTitleFactory({title: "prop prop", color: 'peru'}),
+          MyTitleFactory({title: "ppppppp", color: 'blue'}),
+          MyTitleFactory({title: "ha-ha-ha", color: 'green'})
         ]
       )
     )
