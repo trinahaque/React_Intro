52e64b6d0bd9d38317c4e2a9a70e96f96a64022a
Trina Haque <trina_haque28@outlook.com>
2017-06-22 17:05:20 -0700

    more installation

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,17 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './myTitle'
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-
-var MyTitle = React.createClass({
-  render: function(){
-    return (
-      div(null,
-        h1({style: {color: this.props.color, fontWeight: 'bold'}}, this.props.title)
-      )
-    )
-  }
-})
-
 
 var MyTitleFactory = React.createFactory(MyTitle)
 
---
+++ js/myTitle.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// var React = require('react')
+// synonymous
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+
+var MyTitle = React.createClass({
+  render: function(){
+    return (
+      div(null,
+        h1({style: {color: this.props.color, fontWeight: 'bold'}}, this.props.title)
+      )
+    )
+  }
+})
+
+export default MyTitle
