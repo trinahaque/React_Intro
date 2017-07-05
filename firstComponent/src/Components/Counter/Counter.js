61835dc1f3eb843b0c49575529c9a2fc0be92295
Trina Haque <trina_haque28@outlook.com>
2017-07-05 16:50:53 -0700

    updated button color

--- firstComponent/src/Components/Counter/Counter.js
+++ firstComponent/src/Components/Counter/Counter.js
@@ -1,17 +1,38 @@
 import React from "react"
+import "./Counter.css";
+
 class Counter extends React.Component{
+
   constructor(props){
     super(props);
-    this.state = {counter:0};
+    this.state = {color: "green", counter:0};
   }
+
+
   buttonClick(counter){
     this.setState({counter: this.state.counter + 1});
+    if (this.state.counter == 15){
+      this.setState({color: "red"})
+    }
   }
+
   render(){
+    const color = this.state.color;
+    let button = null;
+    console.log("This is", this);
+    // if (this.state.counter < 15){
+    //   button = <button onClick={(e) => this.buttonClick()}>Click Me</button>
+    // }
+    // else{
+      button = <button onClick={(e) => this.buttonClick()}>Click Me</button>
+    // }
+
     return (
       <div>
         <h1>Hello, {this.props.name}</h1>
-        <button onClick={() => this.buttonClick()}>Click Me</button>
+        <div className={this.state.color}>
+          {button}
+        </div>
         <p>You clicked me {this.state.counter} times</p>
       </div>
     )
