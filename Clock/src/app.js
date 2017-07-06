f9450745b9cd3d16cfc1e0e28d8306092917aedf
Trina Haque <trina_haque28@outlook.com>
2017-07-06 14:54:34 -0700

    clock states

--- Clock/src/app.js
+++ Clock/src/app.js
@@ -3,6 +3,25 @@ import Clock from "./Components/Clock/Clock.js";
 
 
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () =>
+          this.tick(), 1000
+    ); console.log("set interval still running")
+  }
+
   render(){
     return(
       <div>
